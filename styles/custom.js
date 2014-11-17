72811bd93dff4368ae43eaca387d9ca3e11827da
atamannn <atamanyk@gmail.com>
2014-11-17 21:35:26 +0200

    2.1
    Кнопка пропустить появляется с задержкой

--- styles/custom.js
+++ styles/custom.js
@@ -1,11 +1,14 @@
 jQuery(document).ready(function() {
-
+	$('.next-button').delay(4000).show(1000);
 	jQuery('.next-button').click(function() {
 		jQuery('#secondPage').css('display','block');
 		jQuery('#firstPage').css('display','none');
 	});
-	jQuery('.next-button').delay(3000).fadeIn(function() {
+	
+	
+/*	jQuery('.next-button').delay(7000).fadeIn(function() {
 		jQuery('.connect-button').delay(3000).fadeIn(400);
-	});
+	});*/
+
 
 });
